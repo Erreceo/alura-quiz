6c36730d59e0ca1f38c39c5d01cdae4067c5e501
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2021-01-26 11:36:40 -0300

    Eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import db from '../db.json'
-import Head from "next/head";
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import Head from 'next/head';
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -19,41 +19,41 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     min-height: 100vh;
   }
-  #__next {
+  #__next { 
     flex: 1;
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-        <Head>
-            <title>Alura Quiz</title>
-            <link rel="preconnect" href="https://fonts.gstatic.com"/>
-                <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"/>
-                    <meta name="title" content="The legend of zelda"/>
-                    <meta name="description" content=""/>
+      <Head>
+        <title>Alura Quiz</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+        <meta name="title" content="The legend of zelda" />
+        <meta name="description" content="" />
 
-                    <meta property="og:type" content="website"/>
-                    <meta property="og:url" content="https://alura-quiz-red.vercel.app/"/>
-                    <meta property="og:title" content="The legend of zelda"/>
-                    <meta property="og:description" content=""/>
-                    <meta property="og:image" content={db.bg}/>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://alura-quiz-red.vercel.app/" />
+        <meta property="og:title" content="The legend of zelda" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content={db.bg} />
 
-                    <meta property="twitter:card" content="summary_large_image"/>
-                    <meta property="twitter:url" content="https://alura-quiz-red.vercel.app/"/>
-                    <meta property="twitter:title" content="The legend of zelda"/>
-                    <meta property="twitter:description" content=""/>
-                    <meta property="twitter:image" content={db.bg}/>
-        </Head>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://alura-quiz-red.vercel.app/" />
+        <meta property="twitter:title" content="The legend of zelda" />
+        <meta property="twitter:description" content="" />
+        <meta property="twitter:image" content={db.bg} />
+      </Head>
       <ThemeProvider theme={theme}>
-      <GlobalStyle />
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
