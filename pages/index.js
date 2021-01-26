6ad089dd22d24041883e6aac969806e9d85101f7
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2021-01-26 11:32:26 -0300

    Eslint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import styled from 'styled-components'
-import Widget from "../src/components/Widget";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
-import QuizBackground from "../src/components/QuizBackground";
-import Head from "next/head";
-import db from '../db.json'
+import styled from 'styled-components';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import db from '../db.json';
 
 const QuizContainer = styled.div`
   width: 100%;
@@ -15,41 +14,38 @@ const QuizContainer = styled.div`
     margin: auto;
     padding: 15px;
   }
-`
+`;
 
 export default function Home() {
   return (
-         <QuizBackground backgroundImage={db.bg}>
+    <QuizBackground backgroundImage={db.bg}>
 
-                  <QuizContainer>
-                      <Widget>
-                          <Widget.Header>
-                              <h1>
-                                  The legend of zelda
-                              </h1>
-                          </Widget.Header>
-                          <Widget.Content>
-                              <p>
-                                  Lorem ipsum dolor sit amet...
-                              </p>
-                          </Widget.Content>
-                      </Widget>
-                      <Widget>
-                          <Widget.Content>
-                              <h1>
-                                  The legend of zelda...
-                              </h1>
-                              <p>
-                                  Lorem ipsum dolor sit amet...
-                              </p>
-                          </Widget.Content>
-                      </Widget>
-                      <Footer/>
-                  </QuizContainer>
-                  <GitHubCorner projectUrl={'https://github.com/rodrigoXiita'}/>
-              </QuizBackground>
-  )
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>
+              The legend of zelda
+            </h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>
+              Lorem ipsum dolor sit amet...
+            </p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>
+              The legend of zelda...
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet...
+            </p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/rodrigoXiita" />
+    </QuizBackground>
+  );
 }
-
-
-
