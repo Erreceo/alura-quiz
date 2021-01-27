e091d716bf3971ff41394232bf2a02e7bfa17260
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2021-01-26 23:03:43 -0300

    imput button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,15 +2,16 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   width: 100%;
-  background-color: #FF9800;
+  background-color: ${({ disabled }) => (disabled ? '#8C8C8C' : '#FF9800')};
   height: 36px;
   margin-top: 24px;
   border: 0;
   border-radius: 4px;
   box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.12), 0px 2px 2px rgba(0, 0, 0, 0.24);
-  disabled: ${(disabled) => disabled};
+  disabled: ${({ disabled }) => disabled};
   color: white;
   font-weight: 700;
   font-size: 14px;
+  line-height: 17px;
 `;
 export default Button;
